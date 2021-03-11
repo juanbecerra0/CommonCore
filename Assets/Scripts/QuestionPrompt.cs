4eafbfc78e6cc96368bb665fd1b6fb3f0b0931db
Juan Becerra <juanbecerra@u.boisestate.edu>
2021-03-11 15:03:36 -0700

    Question prompt prefab skeleton

---
+++ Assets/Scripts/QuestionPrompt.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionPrompt : MonoBehaviour
+{
+    // Public -------------------------------------------------------------
+
+
+
+    // Members ------------------------------------------------------------
+
+    // UI Elements
+    private Text m_QuestionText;
+    private Button m_TextToSpeechButton;
+
+    // Initialization -----------------------------------------------------
+
+    private void Awake()
+    {
+        // Get references to entities in the object
+        m_QuestionText = transform.Find("QuestionText").GetComponent<Text>();
+        m_TextToSpeechButton = transform.Find("TextToSpeechButton").GetComponent<Button>();
+
+        // Bind event to button clock
+        m_TextToSpeechButton.onClick.AddListener(OnTextToSpeechClicked);
+    }
+
+    // Events
+
+    private void OnTextToSpeechClicked()
+    {
+        // TODO: Implement
+        Debug.Log("TTS Clicked");
+    }
+
+}
