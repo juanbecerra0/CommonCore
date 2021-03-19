e98e384d58538eb24aecb25524a576ca54372019
Juan Becerra <juanbecerra@u.boisestate.edu>
2021-03-18 18:42:45 -0600

    Implemented item replacing

--- Assets/Scripts/DraggableObject.cs
+++ Assets/Scripts/DraggableObject.cs
@@ -37,11 +37,33 @@ public class DraggableObject : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        // Get offset
         m_CurrentOffset = transform.position - Input.mousePosition;
+
+        // Temporarely move object
+        Vector3 tempPosition = m_CurrentOffset + Input.mousePosition;
+        transform.position = m_InitialLocation;
+
+        // Determine if there was a tile underneath
+        RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition, Vector3.forward, 10.0f);
+
+        // Check if raycast hit was a non-static question tile
+        if (hit && hit.transform.gameObject.tag.Equals(TARGET_TAG))
+        {
+            // Get a reference to this question tile
+            QuestionTile tile = hit.transform.GetComponent<QuestionTile>();
+
+            // Reset the tile
+            tile.ResetInstance();
+        }
+
+        // Move object back to original position
+        transform.position = tempPosition;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        // Move around object based off of current offset
         transform.position = m_CurrentOffset + Input.mousePosition;
     }
 
